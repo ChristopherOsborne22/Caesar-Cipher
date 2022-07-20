07465a1c24bf70184a13836595f70ec2e3d5d820
ChristopherOsborne22 <103477725+ChristopherOsborne22@users.noreply.github.com>
2022-07-20 17:55:27 +0700

    Add files via upload

---
+++ Better_CaesarCipher.py
@@ -0,0 +1,42 @@
+# Your next task: decrypt the below
+import string
+try:
+    text = input("Enter your message: ")
+    shift = int(input("Enter the shift value of your choice (any integer will do): "))
+    cipher = ''
+
+    if shift < 1:
+        print("You have entered a wrong shift value.")
+
+    code = 0
+    for char in text:
+        if char.isdigit():
+            cipher += char
+        elif char in string.punctuation:
+            code = ord(char)
+        elif char.isspace():
+            code = ord(" ")
+        elif char.islower():
+            code = ord(char) + shift
+            while code > ord('z'):
+                remainder = code - ord('z') - 1
+                code = ord('a') + remainder
+        elif char.isupper():
+            code = ord(char) + shift
+            while ord('Z') < code <= ord('a') or ord('a') < code <= ord('z') \
+                    or code > ord('z'):
+                remainder = code - ord('Z') - 1
+                code = ord('A') + remainder
+        cipher += chr(code)
+
+    print(cipher)
+except ValueError:
+    print("You have entered an invalid shift value")
+
+
+# The below is for upper-case letter in decryption:
+# elif char.isupper():
+# code = ord(char) - shift
+# while code < ord('A'):
+#     remainder = shift - (ord(char) - ord('A'))
+#     code = ord('Z') - remainder + 1
